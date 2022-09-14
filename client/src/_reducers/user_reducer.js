60443dd787e7bccfd380b90f21c1673f456a7cc3
moon8997 <tjdansrlf@gmail.com>
2022-09-14 21:39:53 +0900

    로그인 페이지 추가

---
+++ client/src/_reducers/user_reducer.js
@@ -0,0 +1,16 @@
+import {
+    LOGIN_USER
+} from '../_actions/types.js'
+
+export default function (state = {}, action){
+    switch (action.type) {
+        case LOGIN_USER:
+            return { ...state, loginSuccess: action.payload }
+            break;
+    
+        default:
+            return state;
+    }
+
+
+}
