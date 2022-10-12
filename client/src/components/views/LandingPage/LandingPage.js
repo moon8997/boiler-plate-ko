18dce73a73177ce9c6c218fb65f2db473c6e61e9
moon8997 <tjdansrlf@gmail.com>
2022-10-12 18:12:06 +0900

    로그아웃기능 추가

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,6 @@
 import React,{ useEffect}  from 'react'
 import axios from 'axios';
-
+import { useNavigate } from "react-router-dom";
 
 function LandingPage() {
 
@@ -9,6 +9,18 @@ function LandingPage() {
     .then(response => console.log(response.data))
   }, [])
   
+  const onClickHandler = () => {
+    axios.get(`/api/users/logout`)
+    .then(response => {
+      if(response.data.success){
+        navigate("/login");
+      } else {
+        alert('로그아웃 하는데 실패 했습니다.')
+      }
+    })
+  }
+
+  const navigate = useNavigate();
 
   return (
     <div style={{
@@ -16,6 +28,11 @@ function LandingPage() {
         , width:'100%', height:'100vh'
     }}>
         <h2>시작 페이지</h2>
+
+        <button onClick={onClickHandler}>
+          로그아웃
+        </button>
+
     </div>
   )
 }
--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -2,9 +2,11 @@ import React, { useState } from 'react'
 import Axios from 'axios'
 import { useDispatch } from 'react-redux';
 import { loginUser } from '../../../_actions/user_action';
+import { useNavigate } from "react-router-dom";
 
 function LoginPage(props) {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     const [Email, setEmail] = useState("")
     const [Password, setPassword] = useState("")
@@ -28,7 +30,7 @@ function LoginPage(props) {
         dispatch(loginUser(body))
             .then(response => {
                 if (response.payload.loginSuccess) {
-                    props.history.push('/')
+                    navigate("/");
                 } else {
                     alert('Error˝')
                 }
